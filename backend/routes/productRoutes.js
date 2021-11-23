ffd581fbd3eaf0042a2df4f9cf02c86d53d9236b
KengiCo <costagliolak@gmail.com>
2021-11-23 12:10:11 +0100

    added controllers folder and moved the logic from productRoutes to producController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,10 @@
 import express from "express";
 const router = express.Router();
-import Product from "../models/productModel.js";
+import { getProducts, getProductById} from "../controllers/productController.js"
 
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-router.get("/", async (req, res) => {
-  const products = await Product.find({});
+router.route("/").get(getProducts)
 
-  res.json(products);
-});
-
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @access Public
-
-router.get("/:id", async (req, res) => {
-  console.log(req.params);
-  const product = await Product.findById(req.params.id);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: "Oooops ... product not found" });
-  }
-});
+router.route("/:id").get(getProductById);
 
 export default router;
